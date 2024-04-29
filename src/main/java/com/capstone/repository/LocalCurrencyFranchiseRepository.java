7f7716bf083068400d8ddd66527c15c9316c99e2
choda1510 <choda1510@kyungmin.ac.kr>
2024-04-29 13:57:32 +0900

    rest controller test coding.

--- src/main/java/com/capstone/repository/LocalCurrencyFranchiseRepository.java
+++ src/main/java/com/capstone/repository/LocalCurrencyFranchiseRepository.java
@@ -3,8 +3,6 @@ package com.capstone.repository;
 import com.capstone.entity.LocalCurrencyFranchise;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
--- src/test/java/com/capstone/controller/LocalCurrencyFranchiseRestControllerTest.java
+++ src/test/java/com/capstone/controller/LocalCurrencyFranchiseRestControllerTest.java
@@ -1,8 +1,41 @@
 package com.capstone.controller;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+import java.math.BigDecimal;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 @SpringBootTest
+@AutoConfigureMockMvc
 public class LocalCurrencyFranchiseRestControllerTest {
+    @Autowired
+    protected MockMvc mockMvc;
+    @Autowired
+    private WebApplicationContext context;
+    @BeforeEach
+    public void beforeMock() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+    @Test
+    public void between() throws Exception {
+        String url = "/franchise?fromLa=37.64373254&toLa=37.64373254&fromLo=127.1415573&toLo=127.1415573";
+        final ResultActions result = mockMvc.perform(get(url).accept(MediaType.APPLICATION_JSON));
+        result
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].latitude").value(new BigDecimal("37.64373254").setScale(30)))
+                .andExpect(jsonPath("$[0].longitude").value(new BigDecimal("127.1415573").setScale(30)));
+    }
 
 }
