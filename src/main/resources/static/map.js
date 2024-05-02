2bac2b177d520ffa6c7efeec23fcaf4ccc81a76f
kysman2580 <kysman2580@kyungmin.ac.kr>
2024-05-02 17:50:03 +0900

    클러스터 기능 추가 인포윈도우 기능,이벤트 추가 동적으로 지도를 조작하는 기능 추가

--- src/main/resources/static/map.js
+++ src/main/resources/static/map.js
@@ -1,49 +1,55 @@
-var mapContainer = document.getElementById('map'), // 지도를 표시할 div
-              mapOption = {
-                  center: new kakao.maps.LatLng(36.2683, 127.6358), // 지도의 중심좌표
-                  level: 14 // 지도의 확대 레벨
-              };
+var mapContainer = document.getElementById('map'),
+    mapOption = {
+        center: new kakao.maps.LatLng(37.566826004661, 126.978652258309), // 초기 지도의 중심좌표
+        level: 12 // 지도의 초기 확대 레벨
+    };
 
-          var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+var map = new kakao.maps.Map(mapContainer, mapOption); // 지도 생성
 
-          // 마커 클러스터러를 생성합니다
-          var clusterer = new kakao.maps.MarkerClusterer({
-              map: map, // 마커들을 클러스터로 관리하고 표시할 지도 객체
-              averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정
-              minLevel: 10 // 클러스터 할 최소 지도 레벨
-          });
+// 마커 클러스터러 생성
+var clusterer = new kakao.maps.MarkerClusterer({
+    map: map,
+    averageCenter: true,
+    minLevel: 10
+});
 
+// 지도 이벤트 리스너 등록, 지도의 범위가 변경되었을 때 실행됩니다.
+kakao.maps.event.addListener(map, 'idle', function() {
+    loadMarkers(); // 지도의 보이는 영역이 변경될 때마다 마커를 로드
+});
 
-          function loadData() {
-              var url = '/franchise?fromLa=37.0&toLa=38.0&fromLo=126.0&toLo=128.0';
-              fetch(url)
-                  .then(response => response.json())
-                  .then(data => {
-                      var markers = data.map(function (franchise) {
-                          var markerPosition = new kakao.maps.LatLng(franchise.latitude, franchise.longitude);
-                          var marker = new kakao.maps.Marker({
-                              position: markerPosition
-                          });
+function loadMarkers() {
+    // 지도의 현재 영역을 얻습니다.
+    var bounds = map.getBounds();
+    var swLatLng = bounds.getSouthWest(); // 남서쪽 좌표
+    var neLatLng = bounds.getNorthEast(); // 북동쪽 좌표
 
-                          // 인포윈도우에 표시될 내용 설정
-                          var infowindow = new kakao.maps.InfoWindow({
-                              content: '<div style="padding:20px;">' +
-                                       '상호명: ' + franchise.name + '<br>' +
-                                       '업종: ' + franchise.sector + '<br>' +
-                                       '주소: ' + franchise.map_address + '</div>',
-                                removable : true
-                          });
+    var url = `/franchise?fromLa=${swLatLng.getLat()}&toLa=${neLatLng.getLat()}&fromLo=${swLatLng.getLng()}&toLo=${neLatLng.getLng()}`;
+                  fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            var markers = data.map(function (franchise) {
+                var markerPosition = new kakao.maps.LatLng(franchise.latitude, franchise.longitude);
+                var marker = new kakao.maps.Marker({
+                    position: markerPosition
+                });
 
-                          // 마커에 클릭 이벤트를 등록합니다
-                          kakao.maps.event.addListener(marker, 'click', function() {
-                              infowindow.open(map, marker); // 마커 클릭 시 인포윈도우를 표시
-                          });
+                var infowindow = new kakao.maps.InfoWindow({
+                    content: '<div style="padding:50px;">' +
+                             '상호명: ' + franchise.name + '<br>' +
+                             '업종: ' + franchise.sector + '<br>' +
+                             '주소: ' + franchise.mapAddress + '</div>',
+                    removable: true
+                });
 
-                          return marker;
-                      });
-                      // 클러스터러에 마커들을 추가합니다
-                      clusterer.addMarkers(markers);
-                  })
-                  .catch(err => console.error('Error loading the franchise data:', err));
-          }
-          window.onload = loadData; // 페이지 로드 시 데이터 로드
+                kakao.maps.event.addListener(marker, 'click', function() {
+                    infowindow.open(map, marker);
+                });
+
+                return marker;
+            });
+            clusterer.clear(); // 이전 마커들을 클리어
+            clusterer.addMarkers(markers); // 새로운 마커들을 클러스터러에 추가
+        })
+        .catch(err => console.error('Error loading the franchise data:', err));
+}
